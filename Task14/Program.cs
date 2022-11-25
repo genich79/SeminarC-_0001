c8930c1c0b6d4310c5971bc1d5f55b149cfd4b1e
Johnny <genich79@gmail.com>
2022-11-25 17:31:29 +0300

    Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,7 @@
+﻿// Task13: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+int n1 = n / 10; 
+int n2 = n1 / 10;
+Console.Write (n1 - n2 * 10 );
