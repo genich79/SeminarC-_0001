d1974732b38dbe1615b5c4c0089e0e990633e52b
Johnny <genich79@gmail.com>
2022-12-06 14:00:52 +0300

    add All

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -3,7 +3,7 @@
 Console.Clear();
 Console.WriteLine("Введите пятизначное число");
 string? n = Console.ReadLine();
-if (n[0] == n[4] && n[1] == n[3])
+if (n?[0] == n?[4] && n?[1] == n?[3])
     Console.WriteLine("yes");
 else
     Console.WriteLine("no");
